f0971facca563c4152f198f44d093f2d5d02cb5f
Pierre Wahlgren <pierre.wahlgren@klarna.com>
2017-01-30 12:43:50 +0100

    Setup React and render simple Lekplats component

---
+++ src/Lekplats/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Lekplats () {
+  return <h1>Lekplats</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import style from './index.css'
+import React from 'react'
+import { render } from 'react-dom'
+import Lekplats from './Lekplats'
 
-const div = document.createElement('div')
-div.innerHTML = '<h1>Hello World</h1>'
-div.className = style.component
-
-document.getElementById('root').appendChild(div)
+render(
+  <Lekplats />,
+  document.getElementById('root')
+)
